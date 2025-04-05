f9c59d1304fd39cbc5cb9f804d8a7c5f909e8f11
Oliver <oschi1303@gmail.com>
2025-04-05 14:54:23 +0200

    ai-worker: add ollama package

--- services/ai-worker/cmd/cmd/main.go
+++ services/ai-worker/cmd/cmd/main.go
@@ -1,9 +1,31 @@
-package cmd
+package main
 
-import "log/slog"
+import (
+	"context"
+	"fmt"
+	"github.com/OliverSchlueter/mauerstrassenloewen/ai-worker/internal/ollama"
+	"log/slog"
+	"os"
+)
 
 func main() {
 	slog.SetLogLoggerLevel(slog.LevelDebug)
+	ctx := context.Background()
 
-	slog.Info("Hello, world!")
+	client, err := ollama.NewClient(ollama.Configuration{
+		BaseURL: "http://localhost:11434",
+		Model:   "deepseek-r1:14b",
+	})
+	if err != nil {
+		slog.Error("failed to create ollama client", "error", err)
+		os.Exit(1)
+	}
+
+	resp, err := client.Chat(ctx, "Schreibe ein Gedicht in 100 Wörtern über Softwareentwicklung.")
+	if err != nil {
+		slog.Error("failed to get response", "error", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
+
 }
---
+++ services/ai-worker/internal/ollama/ollama.go
@@ -0,0 +1,69 @@
+package ollama
+
+import (
+	"context"
+	"fmt"
+	"github.com/ollama/ollama/api"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type Client struct {
+	ollama *api.Client
+	model  string
+}
+
+type Configuration struct {
+	BaseURL string
+	Model   string
+}
+
+func NewClient(cfg Configuration) (*Client, error) {
+	baseURL, err := url.Parse(cfg.BaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse base URL: %w", err)
+	}
+
+	httpClient := &http.Client{
+		Timeout: 10 * time.Minute,
+	}
+
+	ollama := api.NewClient(baseURL, httpClient)
+
+	return &Client{
+		ollama: ollama,
+		model:  cfg.Model,
+	}, nil
+}
+
+func (c *Client) Chat(ctx context.Context, message string) (string, error) {
+	stream := true
+	req := api.GenerateRequest{
+		Model:  c.model,
+		Prompt: message,
+		Stream: &stream,
+		Raw:    false,
+	}
+
+	var resp strings.Builder
+	respFunc := func(gr api.GenerateResponse) error {
+		_, err := resp.WriteString(gr.Response)
+		if err != nil {
+			return fmt.Errorf("failed to write response: %w", err)
+		}
+
+		return nil
+	}
+
+	slog.Info("Generating response", "model", c.model, "prompt", message)
+
+	err := c.ollama.Generate(ctx, &req, respFunc)
+	if err != nil {
+		return "", fmt.Errorf("failed to get response: %w", err)
+	}
+
+	return resp.String(), nil
+}
