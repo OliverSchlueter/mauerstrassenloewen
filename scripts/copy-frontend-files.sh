1bac6007b4bca6675267ac773f3338424812efb1
Oliver <oschi1303@gmail.com>
2025-04-10 18:06:37 +0200

    scripts: Update copy-frontend-files.sh

--- scripts/copy-frontend-files.sh
+++ scripts/copy-frontend-files.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+mkdir -p ../services/backend/internal/frontend/assets
 cp -r ../frontend/dist/frontend/browser/* ../services/backend/internal/frontend/assets
+
+mkdir -p ../services/backend/internal/docs/assets
+cp -r ../docs/.retype/* ../services/backend/internal/docs/assets
