1b3759d4c0d58c01b9415be9dbe1fa735b256e14
Oliver <oschi1303@gmail.com>
2025-04-11 21:40:07 +0200

    Set up reverse proxy

---
+++ services/reverseproxy/cmd/e2e/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/OliverSchlueter/mauerstrassenloewen/common/middleware"
+	"github.com/OliverSchlueter/mauerstrassenloewen/common/sloki"
+	"github.com/OliverSchlueter/mauerstrassenloewen/reverseproxy/internal/backend"
+	"github.com/OliverSchlueter/mauerstrassenloewen/reverseproxy/internal/fflags"
+	"github.com/justinas/alice"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	fflags.EndToEndEnvironment.Enable()
+	//fflags.SendLogsToLoki.Enable()
+
+	// Setup logging
+	lokiService := sloki.NewService(sloki.Configuration{
+		URL:          "http://localhost:3100/loki/api/v1/push",
+		Service:      "reverseproxy",
+		ConsoleLevel: slog.LevelDebug,
+		LokiLevel:    slog.LevelInfo,
+		EnableLoki:   fflags.SendLogsToLoki.IsEnabled(),
+	})
+	slog.SetDefault(slog.New(lokiService))
+
+	mux := http.NewServeMux()
+	port := "8082"
+
+	backend.Start(backend.Configuration{
+		Mux: mux,
+	})
+
+	go func() {
+		chain := alice.New(
+			middleware.Logging,
+			middleware.RecoveryMiddleware,
+		).Then(mux)
+
+		err := http.ListenAndServe(":"+port, chain)
+		if err != nil {
+			slog.Error("Could not start server on port "+port, slog.Any("err", err.Error()))
+			os.Exit(1)
+		}
+	}()
+
+	slog.Info(fmt.Sprintf("Reverse proxy service started on http://localhost:%s", port))
+
+	// Wait for a signal to exit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	switch <-sig {
+	case os.Interrupt:
+		slog.Info("Received interrupt signal, shutting down...")
+
+		slog.Info("Shutdown complete")
+	}
+}
