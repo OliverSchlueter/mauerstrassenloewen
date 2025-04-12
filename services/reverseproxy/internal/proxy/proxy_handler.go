97db581689ab9b243c8de221e1fc7b85c065a427
Oliver <oschi1303@gmail.com>
2025-04-12 09:03:19 +0200

    reverseproxy: Enhance error handling

--- services/reverseproxy/internal/proxy/proxy_handler.go
+++ services/reverseproxy/internal/proxy/proxy_handler.go
@@ -13,10 +13,16 @@ import (
 
 func NewProxy(target *url.URL) *httputil.ReverseProxy {
 	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
+		slog.Warn("Error while proxying request", slog.String("error", err.Error()))
+		http.Error(w, http.StatusText(http.StatusBadGateway), http.StatusBadGateway)
+	}
+
 	return proxy
 }
 
-func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint endpoint.Endpoint) func(http.ResponseWriter, *http.Request) {
+func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, e endpoint.Endpoint) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Request received", sloki.WrapRequest(r))
 
@@ -26,9 +32,9 @@ func RequestHandler(proxy *httputil.ReverseProxy, url *url.URL, endpoint endpoin
 		r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
 
 		path := r.URL.Path
-		r.URL.Path = strings.TrimLeft(path, endpoint.Endpoint)
+		r.URL.Path = strings.TrimPrefix(path, e.Endpoint)
 
-		slog.Info(fmt.Sprintf("Redirecting"), slog.String("endpoint", endpoint.Name), slog.String("to", r.URL.String()))
+		slog.Info(fmt.Sprintf("Redirecting"), slog.String("endpoint", e.Name), slog.String("to", r.URL.String()))
 		proxy.ServeHTTP(w, r)
 	}
 }
