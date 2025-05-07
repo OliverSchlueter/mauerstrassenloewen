fe1aff3bf66efdecd04383d262e410bf5f796c64
Oliver <oschi1303@gmail.com>
2025-05-07 19:35:47 +0200

    backend: Add BSON tags to User model fields for MongoDB compatibility

--- services/backend/internal/usermanagement/model.go
+++ services/backend/internal/usermanagement/model.go
@@ -5,26 +5,26 @@ import (
 )
 
 type User struct {
-	ID                    string    `json:"id"`
-	Email                 string    `json:"e_mail"`
-	Password              string    `json:"password"`
-	LastName              string    `json:"last_name"`
-	FirstName             string    `json:"first_name"`
-	DateOfBirth           time.Time `json:"date_of_birth"`
-	PlaceOfBirth          string    `json:"place_of_birth"`
-	MaritalStatus         string    `json:"marital_status"`
-	Profession            string    `json:"profession"`
-	Citizenship           string    `json:"citizenship"`
-	Degree                string    `json:"degree"`
-	SalaryPerMonth        float32   `json:"salary_per_month"`
-	Assets                string    `json:"assets"`
-	SelfEmployed          bool      `json:"self_employed"`
-	TradingBudgetPerMonth float32   `json:"trading_budget_per_month"`
-	HasTradingExperience  bool      `json:"has_trading_experience"`
-	TradingExperience     string    `json:"trading_experience"`
-	KnowledgeInTrading    string    `json:"knowledge_in_trading"`
-	Motivation            string    `json:"motivation"`
-	ExcludeCompanies      string    `json:"exclude_companies"`
-	RiskTaking            int       `json:"risk_taking"`
-	Preferences           string    `json:"preferences"`
+	ID                    string    `json:"id" bson:"user_id"`
+	Email                 string    `json:"e_mail" bson:"e_mail"`
+	Password              string    `json:"password" bson:"password"`
+	LastName              string    `json:"last_name" bson:"last_name"`
+	FirstName             string    `json:"first_name" bson:"first_name"`
+	DateOfBirth           time.Time `json:"date_of_birth" bson:"date_of_birth"`
+	PlaceOfBirth          string    `json:"place_of_birth" bson:"place_of_birth"`
+	MaritalStatus         string    `json:"marital_status" bson:"marital_status"`
+	Profession            string    `json:"profession" bson:"profession"`
+	Citizenship           string    `json:"citizenship" bson:"citizenship"`
+	Degree                string    `json:"degree" bson:"degree"`
+	SalaryPerMonth        float32   `json:"salary_per_month" bson:"salary_per_month"`
+	Assets                string    `json:"assets" bson:"assets"`
+	SelfEmployed          bool      `json:"self_employed" bson:"self_employed"`
+	TradingBudgetPerMonth float32   `json:"trading_budget_per_month" bson:"trading_budget_per_month"`
+	HasTradingExperience  bool      `json:"has_trading_experience" bson:"has_trading_experience"`
+	TradingExperience     string    `json:"trading_experience" bson:"trading_experience"`
+	KnowledgeInTrading    string    `json:"knowledge_in_trading" bson:"knowledge_in_trading"`
+	Motivation            string    `json:"motivation" bson:"motivation"`
+	ExcludeCompanies      string    `json:"exclude_companies" bson:"exclude_companies"`
+	RiskTaking            int       `json:"risk_taking" bson:"risk_taking"`
+	Preferences           string    `json:"preferences" bson:"preferences"`
 }
