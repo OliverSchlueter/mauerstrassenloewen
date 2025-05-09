bdeed8b9234662e231d0ef96b4ff93bbaa6cc27d
Eryk-Byte <kynast-e@web.de>
2025-05-09 09:40:20 +0200

    backend: expand user-model

--- services/backend/internal/usermanagement/model.go
+++ services/backend/internal/usermanagement/model.go
@@ -1,5 +1,7 @@
 package usermanagement
 
+import "time"
+
 type User struct {
 	ID       string  `json:"id" bson:"user_id"`
 	Name     string  `json:"name" bson:"name"`
@@ -9,7 +11,41 @@ type User struct {
 }
 
 type Profile struct {
-	ID      string `json:"id" bson:"profile_id"`
-	Surname string `json:"surname" bson:"surname"`
-	Name    string `json:"name" bson:"name"`
+	ID                           string    `json:"id" bson:"profile_id"`
+	Surname                      string    `json:"surname" bson:"surname"`
+	Name                         string    `json:"name" bson:"name"`
+	Birthdate                    time.Time `json:"birthdate" bson:"birthdate"`
+	Profession                   string    `json:"profession" bson:"profession"`
+	EmploymentStatus             string    `json:"employmentStatus" bson:"employmentStatus"`
+	WorkHoursFlexibility         string    `json:"workHoursFlexibility" bson:"workHoursFlexibility"`
+	Residence                    string    `json:"residence" bson:"residence"`
+	MartialStatus                string    `json:"martialStatus" bson:"martialStatus"`
+	FinancialObligations         string    `json:"financialObligations" bson:"financialObligations"`
+	PrimaryGoal                  string    `json:"primaryGoal" bson:"primaryGoal"`
+	TimeHorizon                  string    `json:"timeHorizon" bson:"timeHorizon"`
+	StartingCapital              string    `json:"startingCapital" bson:"startingCapital"`
+	MonthlyInvestment            string    `json:"monthlyInvestmentn" bson:"monthlyInvestment"`
+	AnnualProfitTarget           string    `json:"annualProfitTarget" bson:"annualProfitTarget"`
+	PreferredMarkets             string    `json:"preferredMarkets" bson:"preferredMarkets"`
+	ToolsPlatforms               string    `json:"toolsPlatforms" bson:"toolsPlatforms"`
+	WeekyTimeCommitment          string    `json:"weekyTimeCommitment" bson:"weekyTimeCommitmenty"`
+	TradingExperience            string    `json:"tradingExperience" bson:"tradingExperience"`
+	PastPerformance              string    `json:"pastPerformance" bson:"pastPerformance"`
+	ChartAnalysisKnowledge       string    `json:"chartAnalysisKnowledge" bson:"chartAnalysisKnowledge"`
+	FundamentalAnalysisKnowledge string    `json:"fundamentalAnalysisKnowledge" bson:"fundamentalAnalysisKnowledge"`
+	Certifications               string    `json:"certifications" bson:"certifications"`
+	RiskType                     string    `json:"riskType" bson:"riskType"`
+	LossBehavior                 string    `json:"lossBehavior" bson:"lossBehavior"`
+	Discipline                   string    `json:"discipline" bson:"discipline"`
+	TradingJournal               bool      `json:"tradingJournal" bson:"tradingJournal"`
+	StressTolerance              string    `json:"stressTolerance" bson:"stressTolerance"`
+	EsgPreferences               string    `json:"esgPreferences" bson:"esgPreferences"`
+	ExclusionCriteria            string    `json:"exclusionCriteria" bson:"exclusionCriteria"`
+	PoliticalInvesting           bool      `json:"politicalInvesting" bson:"politicalInvesting"`
+	ReligiousRestrictions        string    `json:"religiousRestrictions" bson:"religiousRestrictions"`
+	BrokerFeesModel              string    `json:"brokerFeesModel" bson:"brokerFeesModel"`
+	TaxSituation                 string    `json:"taxSituation" bson:"taxSituation"`
+	Bookkeeping                  string    `json:"bookkeeping" bson:"bookkeeping"`
+	AutomationInterest           bool      `json:"automationInterest" bson:"automationInterest"`
+	AvailabeTech                 string    `json:"availabeTech" bson:"availabeTech"`
 }
