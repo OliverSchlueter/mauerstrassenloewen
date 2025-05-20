2507a2815253bcb6d38d426e863c9d04dcf806fb
henning.franzen <henning.franzen@flender.com>
2025-05-20 11:09:50 +0200

    feat(register): Add registration form with validation and styling enhancements

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
 import {InterviewComponent} from '../interview/interview.component';
 import {User} from '../models/User';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   imports: [
-    InterviewComponent
+    InterviewComponent,
+    MatTabGroup,
+    MatTab,
+    MatCard,
+    MatCardTitle,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    FormsModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  user= new User()
+  repeatPassword = '';
 
   register() {
 
