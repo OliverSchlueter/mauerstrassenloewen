edc7f33364e07cc99e563b8539b755a4a7aa0b37
Oliver <oschi1303@gmail.com>
2025-05-23 09:07:49 +0200

    common: Run next handler in CORS middleware

--- services/common/middleware/cors_middleware.go
+++ services/common/middleware/cors_middleware.go
@@ -5,5 +5,6 @@ import "net/http"
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
 	})
 }
