0884bf58c4678834158cad8d2af68376b19e36ea
henning <GrievousH@posteo.de>
2025-05-23 14:27:30 +0200

    feat(frontend): added chat

--- frontend/src/app/coach/coach.component.ts
+++ frontend/src/app/coach/coach.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {NgClass, NgFor, NgIf} from '@angular/common';
+import {Message} from '../models/Message'
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-coach',
-  imports: [],
+  imports: [
+    NgFor,
+    NgClass,
+    MatIconButton,
+    MatIcon,
+    NgIf
+  ],
   templateUrl: './coach.component.html',
+  standalone: true,
   styleUrl: './coach.component.scss'
 })
 export class CoachComponent {
+  messages: Message[] = [
+    {
+      content: "testesetsetsetsetsetsetsetsetset",
+      fromUser: true,
+    },
+    {
+      content: "loplolololololololololololololol",
+      fromUser: false,
+    },
+    {
+      content: "so uncivilized",
+      fromUser: true,
+    },
+  ]
 
+  getClass(message: Message) {
+    if(message.fromUser) {
+      return 'fromUser'
+    } else {
+      return 'fromBot'
+    }
+  }
 }
