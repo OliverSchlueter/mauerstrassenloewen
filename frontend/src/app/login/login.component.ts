b4fde52474a02fbd56bd4fc1699561fef2ebcc6b
henning.franzen <henning.franzen@flender.com>
2025-06-10 15:09:54 +0200

    feat(login): Enhance password input with toggle visibility and update risk affinity slider options. login not really working

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {MatError, MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {Component, signal} from '@angular/core';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatError, MatFormField, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
 import {Router} from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {NgIf} from '@angular/common';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
@@ -19,13 +20,17 @@ import {NgIf} from '@angular/common';
     MatCardContent,
     MatCardTitle,
     MatError,
-    NgIf
+    NgIf,
+    MatIcon,
+    MatSuffix,
+    MatIconButton,
   ],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  hidePassword = signal(true)
   errorMessage: string = "";
   username: string = "";
   password: string = "";
@@ -45,5 +50,10 @@ export class LoginComponent {
 
   register() {
     this.router.navigate(['register'])
+  }
+
+  clickPassword($event: MouseEvent) {
+    this.hidePassword.set(!this.hidePassword);
+    $event.stopPropagation()
   }
 }
