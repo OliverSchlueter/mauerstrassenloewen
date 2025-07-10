93f7bd8dcf43ad7350e34e88c9db22c130c5a04a
henning <GrievousH@posteo.de>
2025-07-10 17:24:18 +0200

    feat(frontend): add modules theory

--- frontend/src/app/theory/module/module.component.ts
+++ frontend/src/app/theory/module/module.component.ts
@@ -3,19 +3,58 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {NgIf} from '@angular/common';
 import {MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatDivider} from '@angular/material/divider';
 
 @Component({
   selector: 'app-module',
   imports: [
     NgIf,
     MatIconButton,
-    MatIcon
+    MatIcon,
+    MatExpansionModule,
+    MatCardHeader,
+    MatCardContent,
+    MatDivider,
+    MatCard,
   ],
   templateUrl: './module.component.html',
+  standalone: true,
   styleUrl: './module.component.scss'
 })
 export class ModuleComponent implements OnInit{
   moduleNo = ""
+  modules = [
+    {
+      index: "1",
+      title: "The Basics"
+    },
+    {
+      index: "2",
+      title: "The Market"
+    },
+    {
+      index: "3",
+      title: "The First Step"
+    },
+    {
+      index: "4",
+      title: "The Choice"
+    },
+    {
+      index: "5",
+      title: "The Golden Rule"
+    },
+    {
+      index: "6",
+      title: "The Strategy"
+    },
+    {
+      index: "7",
+      title: "The Borders"
+    },
+  ]
 
   constructor(private route: ActivatedRoute, private router: Router) {
   }
@@ -26,5 +65,10 @@ export class ModuleComponent implements OnInit{
 
   navigateBack() {
     this.router.navigate(["/theory"]);
+  }
+
+  getHeader(moduleNo: string) {
+    const number = <number><unknown>moduleNo;
+    return this.modules[number-1].title
   }
 }
